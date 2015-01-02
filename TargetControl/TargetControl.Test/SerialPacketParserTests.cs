83564ebdb1987899863a18f9eb3f07bcad5d43a7
Kevin Hjelden <github@burntpopcorn.net>
2015-01-02 11:13:59 -0800

    Add serial protocol handler & tests

---
+++ TargetControl/TargetControl.Test/SerialPacketParserTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace TargetControl.Test
+{
+    [TestFixture]
+    public class SerialPacketParserTests
+    {
+        private SerialPacketParser _parser;
+        private Mock<ISerialPacketHandler> _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _parser = new SerialPacketParser();
+            _handler = new Mock<ISerialPacketHandler>();
+
+            _handler.Setup(x => x.CheckPacket(It.IsAny<string>()))
+                .Returns((int?)null);
+        }
+
+        [Test]
+        public void WhenAddingOneByte_ExpectPacketCheckCalled()
+        {
+            _parser.AddData("1", _handler.Object);
+
+            _handler.Verify(x => x.CheckPacket("1"));
+        }
+
+        [Test]
+        public void WhenHandlerReturnsPacketLength_ExpectDataRemoved()
+        {
+            _handler.Setup(x => x.CheckPacket("1234")).Returns(4);
+
+            _parser.AddData("1234", _handler.Object);
+            _parser.AddData("5678", _handler.Object);
+
+            _handler.Verify(x => x.CheckPacket("5678"));
+        }
+
+        [Test]
+        public void WhenAddingByteByByte_ExpectWholeString()
+        {
+            _parser.AddData("1", _handler.Object);
+            _parser.AddData("2", _handler.Object);
+
+            _handler.Verify(x => x.CheckPacket("12"));
+        }
+    }
+}
---
+++ TargetControl/TargetControl/Models/SerialCommandInterface.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetControl
+{
+    public class SerialCommandInterface : ISerialPacketHandler
+    {
+        private ISerial _serial;
+        private readonly ISerialPacketParser _serialParser;
+        private char _lastAddress;
+        private char _lastDevice;
+        private char _lastDataH;
+        private char _lastDataL;
+
+        public SerialCommandInterface(ISerial serial, ISerialPacketParser serialParser)
+        {
+            _serial = serial;
+            _serialParser = serialParser;
+
+            _serial.SerialDataReceived += OnSerialDataReceived;
+        }
+
+        public event Action<SCIReadData> DataReceived = delegate { }; 
+
+        public void Read(char address, char device)
+        {
+            var buf = string.Format("{{{0}{1}}}", address, device);
+            _serial.SendPacket(buf);
+            _lastAddress = address;
+            _lastDevice = device;
+        }
+
+        public void Write(char address, char device, char dataH, char dataL)
+        {
+            var buf = string.Format("[{0}{1}{2}{3}]", address, device, dataH, dataL);
+            _serial.SendPacket(buf);
+            _lastAddress = address;
+            _lastDevice = device;
+            _lastDataH = dataH;
+            _lastDataL = dataL;
+        }
+
+        public int? CheckPacket(string buf)
+        {
+            if (buf.Length == 0)
+            {
+                return null;
+            }
+
+            if (buf[0] == '(')
+            {
+                if (buf.Length < 4)
+                {
+                    return null;
+                }
+
+                if (buf[3] != ')')
+                {
+                    return -1;
+                }
+
+                DataReceived(new SCIReadData
+                {
+                    Address = _lastAddress,
+                    Device = _lastDevice,
+                    DataH = buf[1],
+                    DataL = buf[2]
+                });
+                return 4;
+            }
+
+            if (buf[0] == '<')
+            {
+                if (buf.Length < 3)
+                {
+                    return null;
+                }
+
+                if (buf[1] != _lastAddress || buf[2] != '>')
+                {
+                    return -1;
+                }
+
+                DataReceived(new SCIReadData
+                {
+                    Address = _lastAddress,
+                    Device = _lastDevice,
+                    DataH = _lastDataH,
+                    DataL = _lastDataL
+                });
+                return 3;
+            }
+
+            return -1;
+        }
+
+        private void OnSerialDataReceived(string data)
+        {
+            _serialParser.AddData(data, this);
+        }
+    }
+
+    public struct SCIReadData
+    {
+        public char Address { get; set; }
+        public char Device { get; set; }
+        public char DataH { get; set; }
+        public char DataL { get; set; }
+    }
+}
